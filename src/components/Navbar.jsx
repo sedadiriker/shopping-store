783f67664a89ca19f4b0b37d87e5feba91e01488
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-24 10:36:34 +0300

    create hamburger menu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, {  useState } from "react";
+import logo from "../assets/logo.jpg";
+import { NavLink, useLocation } from "react-router-dom";
+import { closeNavbar, logoutIcon, openNavbar } from "../helpers/icons";
+
+const navigation = [
+  {
+    title: "Home",
+    path: "/dashboard",
+  },
+  {
+    title: "Products",
+    path: "/dashboard/products",
+  },
+  {
+    title: "About",
+    path: "/dashboard/about",
+  },
+];
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const location = useLocation();
+  console.log(location);
+  return (
+    <nav className="bg-navbarColor md:text-sm">
+      <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+        <div className="flex items-center justify-between py-5 md:block ">
+            <img src={logo} alt="logo" width={50} height={50} className="rounded-2xl" />   
+          {/**icon md ekranlardan sonra gizlensin diyoruz */}
+          <div className="md:hidden">
+            <button
+              className="text-gray-500 hover:text-gray-800"
+              onClick={() => setShow(!show)}
+            >
+              {show ? closeNavbar : openNavbar}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`${
+            show ? "flex flex-col pb-2" : "hidden"
+          } md:flex md:flex-row flex-1 items-center `}
+        >
+          <ul className=" md:flex md:space-x-6">
+            {navigation.map((item) => (
+              <li
+                key={item.title}
+                className="text-gray-700 font-medium flex justify-center"
+              >
+                <NavLink
+                  to={item.path}
+                  className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white ${
+                    location.pathname === item.path ? "underline scale-125" : ""
+                  } `}
+                >
+                  {item.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+            <NavLink
+              // to="/"
+              className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white active:bg-gray-900 rounded-full md:inline-flex"
+            >
+              Logout {logoutIcon}
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
